f5f436f0e8e5bf1b6ae8a08f5f19ae31b0699fce
Philip Lundrigan <lundrigan@byu.edu>
2025-03-10 08:59:39 -0600

    Initial commit

---
+++ keymaps/default/macros.h
@@ -0,0 +1,32 @@
+// macros.h
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+// MACROS ROW 1
+#define M00 LGUI(KC_ESCAPE)
+#define M01 KC_NO
+#define M02 KC_NO
+#define M03 KC_NO
+
+// MACROS ROW 2
+#define M10 KC_NO
+#define M11 KC_NO
+#define M12 KC_NO
+#define M13 KC_NO
+
+// MACROS ROW 3
+#define M20 KC_NO
+#define M21 KC_NO
+#define M22 KC_NO
+#define M23 KC_NO
+
+// MACROS ENCODER 1
+#define ENC01 KC_VOLD
+#define ENC02 KC_VOLU
+#define ENC03 KC_MUTE
+
+// MACROS ENCODER 2
+#define ENC10 RGB_MOD
+#define ENC11 RGB_RMOD
+#define ENC12 KC_NO
