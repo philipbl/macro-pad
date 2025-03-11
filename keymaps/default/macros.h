bdf03b0ea3fcc90b2dfc7ac77211245e60d82c5c
Philip Lundrigan <lundrigan@byu.edu>
2025-03-10 20:29:49 -0600

    Make macros more generic

--- keymaps/default/macros.h
+++ keymaps/default/macros.h
@@ -4,7 +4,7 @@
 #include QMK_KEYBOARD_H
 
 // MACROS ROW 1
-#define M00 LGUI(KC_ESCAPE)
+#define M00 KC_NO
 #define M01 KC_NO
 #define M02 KC_NO
 #define M03 KC_NO
@@ -22,11 +22,11 @@
 #define M23 KC_NO
 
 // MACROS ENCODER 1
-#define ENC01 KC_VOLD
-#define ENC02 KC_VOLU
+#define ENC01 KC_NO
+#define ENC02 KC_NO
 #define ENC03 KC_MUTE
 
 // MACROS ENCODER 2
-#define ENC10 RGB_MOD
-#define ENC11 RGB_RMOD
-#define ENC12 KC_NO
+#define ENC10 KC_NO
+#define ENC11 KC_NO
+#define ENC12 KC_MUTE
