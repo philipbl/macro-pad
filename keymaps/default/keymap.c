095a42bcf90d0e27940db5a33708af5151e92fc3
Philip Lundrigan <lundrigan@byu.edu>
2025-03-10 20:29:01 -0600

    Fix encoder variables

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,7 +4,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(ENC03, KC_NO, KC_NO, ENC12, M00, M01, M02, M03, M10, M11, M12, M13, M20, M21, M22, M23)};
 
 #ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {[0] = {ENCODER_CCW_CW(ENC01, ENC02), ENCODER_CCW_CW(ENC11, ENC12)}};
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {[0] = {ENCODER_CCW_CW(ENC01, ENC02), ENCODER_CCW_CW(ENC10, ENC11)}};
 #endif
 
 bool dip_switch_update_user(uint8_t index, bool active) {
